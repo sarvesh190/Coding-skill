329d21b6b068d39e1798366b99d1c5890ec48679
Sarvesh M <138128925+sarvesh190@users.noreply.github.com>
2024-01-19 20:36:48 +0530

    Plus One.py

---
+++ Plus One.py
@@ -0,0 +1,26 @@
+class Solution(object):
+def plusOne(self, digits):
+  """
+  :type digits: List[int]
+  :rtype: List[int]
+  """
+  if len(digits) <= 0:
+    return [0]
+  else:
+    carry = 1
+    i = len(digits)-1
+    running_sum = 0
+    new_digits = []
+  while i >= 0:
+    running_sum = digits[i] + carry
+    if running_sum >= 10:
+      carry = 1
+    else:
+      carry = 0
+      new_digits.append(running_sum % 10)
+      i -= 1
+      if carry == 1:
+        new_digits.append(1)
+        return new_digits[::-1]
+      else:
+        return new_digits[::-1]
