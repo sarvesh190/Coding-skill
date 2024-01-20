697df5a2ef43f5072277f8379a2f88880d96daad
Sarvesh M <138128925+sarvesh190@users.noreply.github.com>
2024-01-20 11:51:55 +0530

    Area of a Circle.py

--- Area of a Circle.py
+++ Area of a Circle.py
@@ -4,10 +4,3 @@ def findArea(r):
  
 print("Area is %.6f" % findArea(5));
 
-9)Python Program to Find the Factorial of a Number
-solu:-def factorial(n):
-     
-    return 1 if (n==1 or n==0) else n * factorial(n - 1) 
-
-num = 5
-print("Factorial of",num,"is",factorial(num))
